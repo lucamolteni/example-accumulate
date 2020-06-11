54683ff88c0df8f6f977e0f13c6f1ad010dd8567
Luca Molteni <volothamp@gmail.com>
2020-06-11 10:51:58 +0200

    Updated gitignore

---
+++ src/main/java/org/kie/firealarm/TestFunction.java
@@ -0,0 +1,52 @@
+package org.kie.firealarm;
+
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.io.Serializable;
+
+import org.kie.api.runtime.rule.AccumulateFunction;
+
+public class TestFunction implements AccumulateFunction<Serializable> {
+
+    @Override
+    public void readExternal(final ObjectInput in) {
+    }
+
+    @Override
+    public Serializable createContext() {
+        return null;
+    }
+
+    @Override
+    public void init(final Serializable context) {
+    }
+
+    @Override
+    public void accumulate(final Serializable context, final Object value) {
+    }
+
+    @Override
+    public void reverse(final Serializable context, final Object value) {
+    }
+
+    @Override
+    public Object getResult(final Serializable context) {
+        return 1;
+    }
+
+    @Override
+    public boolean supportsReverse() {
+        return true;
+    }
+
+    @Override
+    public Class<?> getResultType() {
+        return Number.class;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput objectOutput) throws IOException {
+
+    }
+}
